e88bf4a9e6cd3e21854e897c9c6f923a9cc9e3ce
aakbarkhan <aakbarkhan@gmail.com>
2022-04-22 23:10:18 +0530

    Fixed linters

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,11 +1,11 @@
 import React from "react";
-import {connect} from "react-redux";
-import {createStructuredSelector } from 'reselect';
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 const GET_THINGS_REQUEST = "GET_THINGS_REQUEST";
 export const GET_THINGS_SUCCESS = "GET_THINGS_SUCCESS";
 
-function getThings(){
-  console.log("getThings()Action!")
+function getThings() {
+  console.log("getThings()Action!");
   return (dispatch) => {
     dispatch({ type: GET_THINGS_REQUEST });
     return fetch(`http://localhost:3000/greetings`)
@@ -13,7 +13,7 @@ function getThings(){
       .then((json) => dispatch(getThingsSuccess(json)))
       .catch((error) => console.log(`Fetching Error ${error}`));
   };
-};
+}
 
 export function getThingsSuccess(json) {
   return {
@@ -22,23 +22,26 @@ export function getThingsSuccess(json) {
   };
 }
 class HelloWorld extends React.Component {
-  render () {
-    const { greetings} = this.props
-    const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+  render() {
+    const { greetings } = this.props;
+    const randomGreeting =
+      greetings[Math.floor(Math.random() * greetings.length)];
     return (
       <React.Fragment>
         Greeting: <p>{randomGreeting.name}</p>
         <br></br>
-        <button className="getThingsBtn" onClick={()=> this.props.getThings()}>Greet Me</button>
+        <button className="getThingsBtn" onClick={() => this.props.getThings()}>
+          Greet Me
+        </button>
       </React.Fragment>
     );
   }
 }
 
 const structuredSelector = createStructuredSelector({
-  greetings:state =>state.greetings
+  greetings: (state) => state.greetings,
 });
 
-const mapDispatchToProps = {getThings};
+const mapDispatchToProps = { getThings };
 
 export default connect(structuredSelector, mapDispatchToProps)(HelloWorld);
