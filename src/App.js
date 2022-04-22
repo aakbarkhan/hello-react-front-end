e7afcb4d22c544efebaa29febbcb7d9eb75a4804
aakbarkhan <aakbarkhan@gmail.com>
2022-04-22 23:19:45 +0530

    Fixed linters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import {BrowserRouter as Router,Route,Routes}  from 'react-router-dom';
-import HelloWorld from './components/HelloWorld'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom';
+import HelloWorld from './components/HelloWorld';
 import configureStore from './redux/configureStore';
-import { Provider } from "react-redux";
+import {
+  Provider
+} from "react-redux";
 const store = configureStore();
-
-console.log(store)
 
 class App extends React.Component {
   render () {
